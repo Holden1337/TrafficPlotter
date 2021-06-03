c10f63bbb04d88495bf51447be85a430167390fe
holden1337 <ruegger.2@osu.edu>
2021-06-03 18:09:31 -0400

    changing some things

--- trafficplot.py
+++ trafficplot.py
@@ -12,7 +12,7 @@ gmaps = googlemaps.Client(key ='#Enter your google maps api key here' )
 API = #Enter api key here again
 
 #enter in apartment/home address and work address
-start_address = # "#Enter your start address here "
+start_address = # "Enter your start address here "
 end_address = # "Enter your end address here"
 
 #get latitude and longitude for both start and end address
@@ -22,6 +22,7 @@ start_lat = start_geocode[0]["geometry"]["location"]["lat"]
 start_lng = start_geocode[0]["geometry"]["location"]["lng"]
 
 start = str(start_lat) + ',' + str(start_lng)
+start_temp = start
 
 end_geocode = gmaps.geocode(end_address)
 
@@ -29,13 +30,20 @@ end_lat = end_geocode[0]["geometry"]["location"]["lat"]
 end_lng = end_geocode[0]["geometry"]["location"]["lng"]
 
 end = str(end_lat) + ',' + str(end_lng)
+end_temp = end
 
 #hours you consider to be rush hour or times you might go to or from work
-am, pm = [6,7,8],[15,16,17]
+rush_hours = [6,7,8,15,16,17]
 commute_time = []
 
 while True:
-    if datetime.now().hour in am or datetime.now().hour in pm:
+    if datetime.now().hour in rush_hours:
+        if datetime.now().hour < 12:
+            start = start
+            end = end
+        else:
+            start = end_temp
+            end = start_temp
         url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + start + "&destinations=" + end + "&mode=driving&traffic_model=best_guess&departure_time=now&language=en-EN&sensor=false&key=" + API
         result = simplejson.load(urllib.request.urlopen(url))
         driving_time_seconds = result['rows'][0]['elements'][0]['duration_in_traffic']['value']
@@ -43,7 +51,7 @@ while True:
         commute_time.append(minutes)
 
         #once rush hour is over, make plot of commute time vs time
-        if len(commute_time)>175:
+        if datetime.now().minute == 59 and datetime.now().hours+1 not in rush_hours:
             if datetime.now().hour < 12:
                 mode = 'Morning'
             else:
@@ -55,9 +63,8 @@ while True:
             plt.title('Traffic for ' + mode + ' of ' + str(datetime.now().month) + '/'+ str(datetime.now().day))
             plt.grid(True)
             plt.savefig(str(datetime.now().month) + '-' + str(datetime.now().day) +'-'+ mode + '.png')
-            break
         sleep(60)
     else:
         os.system('clear')
-        print("Not collecting data right now; current time is " +str(datetime.now().hour) + ":" + str(datetime.now().minute))
+        print("Not collecting data right now; waiting for rush hour")
         sleep(60)
